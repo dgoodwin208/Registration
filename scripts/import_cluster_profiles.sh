12d290f2b7c36a63538d06f2a30d29cfd10fe039
Atsushi Kajita <kajita@fixstars.com>
2017-05-22 17:53:55 -0400

    fixed a bug of if-condition and path

--- scripts/import_cluster_profiles.sh
+++ scripts/import_cluster_profiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-matlab -nosplash -nodisplay -r "p=parallel.clusterProfiles; if ~find(ismember(p,'local_96workers')); profile = parallel.importProfile('tests/cluster_profile__local_96workers'); parallel.defaultClusterProfile(profile); else disp('already imported.'); end; exit"
+matlab -nosplash -nodisplay -r "p=parallel.clusterProfiles; if sum(ismember(p,'local_96workers'))==0; profile = parallel.importProfile('scripts/cluster_profile__local_96workers'); parallel.defaultClusterProfile(profile); else disp('already imported.'); end; exit"
 
